b423885231b7af89947a09b188189fca09a643ce
Stacy Mackin <stacy.mackin@gmail.com>
2019-03-31 14:23:56 -0400

    refactor: Refactor JS in script.js to be class based

--- public/js/script.js
+++ public/js/script.js
@@ -1,139 +1,135 @@
 (function() {
 	document.addEventListener('DOMContentLoaded', () => {
-		initSearchControls();
-		initResultListControls();
+		const resultLists = Array.from(document.querySelectorAll('.list-container'));
+		const inputGroups = document.querySelectorAll('.input-group');
+		let thisSearchTerm;
+		let thisResultList;
+
+		/**
+		 * Initialize Search classes
+		 */
+		inputGroups.forEach(thisInputGroup => {
+			thisSearchTerm = thisInputGroup.querySelector('.search').value;
+			thisResultList = resultLists.filter(rl => rl.getAttribute('data-search').toLowerCase() === thisSearchTerm.toLowerCase());
+			new Search(...thisResultList, thisInputGroup);	
+		});
+
+		/**
+		 * Registers .result-list elements as draggable with "Move" button.
+		 */
+		dragula([document.querySelector('.result-lists')],  { 
+			moves: function (el, container, handle) {
+				return handle.classList.contains('js-move');
+			}
+		});
 	});
+  
+	class ResultList {
+		constructor(elm, inputGroup) {
+			this._elm = elm;
+			this._inputGroup = inputGroup;
+			this._toggleSearchResultsBtn = this._elm.querySelector('.js-toggle-search-results');
+			this._toggleDescriptionsBtn = this._elm.querySelector('.js-toggle-descriptions');
+			this._removeBtn = this._elm.querySelector('.js-remove-from-list');
+			this.addEventListeners();
+		}
 
- 	/**
-	 * Add click handling to buttons rendered server side.
- 	 */
-	const initSearchControls = () => {
-		const resultLists = document.querySelector('.result-lists');
-		let thisInput;
-		let mainTerm;
-		let newTerm;
-
-		let addMain = e => {
-			e.preventDefault();
-			mainTerm = document.querySelector(`input[name="main"]`).value;
-			newTerm = thisInput.value;
-			thisInput.value = mainTerm !== "" ? `${mainTerm} ${newTerm}` : newTerm;
-		};
-
-		// let combineSearches = e => {
-		// 	e.preventDefault();
-		// 	mainTerm = document.querySelector(`input[name="main"]`).value;
-		// 	newTerm = thisInput.value;
-		// 	thisInput.value = mainTerm !== "" ? `${mainTerm} ${newTerm}` : newTerm;
-		// };
-
-		document.querySelectorAll('.input-group').forEach(elm => {
-			thisInput = elm.querySelector('input[type="text"]');
-			elm.querySelector('.js-add-main').addEventListener('click', e => {
-				addMain(e);
-			});
-	
-			elm.querySelector('.js-remove-from-search').addEventListener('click', function() {
-				Array.from(document.querySelectorAll('.list-container')).forEach(list => {
-					if (list.getAttribute('data-search').toLowerCase() === this.getAttribute('data-remove').toLowerCase()) {
-						list.remove();
-					}
+		/**
+		 * Adds hide / show toggle for search results.
+		 */
+		toggleSearchResults() {
+			if (this._toggleSearchResultsBtn.getAttribute('data-collapse') === 'show') {
+				this._toggleSearchResultsBtn.setAttribute('data-collapse', 'hide');
+				this._toggleSearchResultsBtn.innerHTML = '<img src="/icons/chevron-up.svg">';
+				this._elm.querySelectorAll('.result-item').forEach(el => {
+					el.style.display = "none";
 				});
-				elm.remove();
-			});
-		});
-
-	 /**
-	  * Toggles result lists as row or column
- 	  */
-		document.querySelectorAll('.js-toggle-list-orientation').forEach(elm => {
-			elm.addEventListener('click', function() {
-				if (this.value === 'row') {
-					resultLists.classList.add('flex-row');
-					resultLists.classList.remove('flex-column');
-				}
-				else if (this.value === 'column') {
-					resultLists.classList.add('flex-column');
-					resultLists.classList.remove('flex-row');
-				}
-			});
-		});
-	};
-
-	/**
-	 * Adds remove from list handling.
-	 */
-	document.querySelectorAll('.js-remove-from-list').forEach(elm => {
-		elm.addEventListener('click', function() {
-			Array.from(document.querySelectorAll('.search')).forEach(searchInput => {
-				if (searchInput.value.toLowerCase() === elm.getAttribute('data-search-input-val').toLowerCase()) {
-					searchInput.closest('.input-group').remove();
-				}
-				elm.closest('.list-container').remove();
-			});
-		});
-	});
-
-	/**
-	 * Adds hide / show toggle for search results.
- 	 */
-	const toggleSearchResults = (resultList, toggleElm) => {
-		if (toggleElm.getAttribute('data-collapse') === 'show') {
-			toggleElm.setAttribute('data-collapse', 'hide');
-			toggleElm.innerHTML = '<img src="/icons/chevron-up.svg">';
-			resultList.querySelectorAll('.result-item').forEach(el => {
-				el.style.display = "none";
-			});
+			}
+			else {
+				this._toggleSearchResultsBtn.setAttribute('data-collapse', 'show');
+				this._toggleSearchResultsBtn.innerHTML = '<img src="/icons/chevron-down.svg">';
+				this._elm.querySelectorAll('.result-item').forEach(el => {
+					el.style.display = "block";
+				});
+			}
 		}
-		else {
-			toggleElm.setAttribute('data-collapse', 'show');
-			toggleElm.innerHTML = '<img src="/icons/chevron-down.svg">';
-			resultList.querySelectorAll('.result-item').forEach(el => {
-				el.style.display = "block";
-			});
-		}
-	};
 
-	/**
-	 * Adds hide / show toggle for search results descriptions.
- 	 */
-	const toggleDescriptions = (toggleElm, resultList) => {
-		toggleElm.addEventListener('click', function() {
-			if (this.getAttribute('data-descriptions') === 'hide') {
-				this.setAttribute('data-descriptions', 'show');
-				this.innerHTML= '<img src="/icons/toggle-right.svg">';
-				resultList.querySelectorAll('.description').forEach(function(description) {
+		/**
+		 * Adds hide / show toggle for search results descriptions.
+		 */
+		toggleDescriptions() {
+			if (this._toggleDescriptionsBtn.getAttribute('data-descriptions') === 'hide') {
+				this._toggleDescriptionsBtn.setAttribute('data-descriptions', 'show');
+				this._toggleDescriptionsBtn.innerHTML= '<img src="/icons/toggle-right.svg">';
+				this._elm.querySelectorAll('.description').forEach(function(description) {
 					description.style.display = 'block';
 				});
 			}
 			else {
-				this.setAttribute('data-descriptions', 'hide');
-				this.innerHTML = '<img src="/icons/toggle-left.svg">';
-				resultList.querySelectorAll('.description').forEach(function(description) {
+				this._toggleDescriptionsBtn.setAttribute('data-descriptions', 'hide');
+				this._toggleDescriptionsBtn.innerHTML = '<img src="/icons/toggle-left.svg">';
+				this._elm.querySelectorAll('.description').forEach(function(description) {
 					description.style.display = 'none';
 				});
 			}
-		});
-	};
+		}
 
-	/**
-	 * Applies events listeners to search result list controls.
- 	 */
-	const initResultListControls = () => {
-		const resultLists = document.querySelectorAll('.result-list');
-		resultLists.forEach(list => {
-			list.querySelector('.js-toggle-search-results').addEventListener('click', function() {
-				toggleSearchResults(list, this);
+		remove() {
+			this._elm.parentNode.removeChild(this._elm);
+		}
+
+		addEventListeners() {
+			this._toggleSearchResultsBtn.addEventListener('click', () => this.toggleSearchResults());
+			this._toggleDescriptionsBtn.addEventListener('click', () => this.toggleDescriptions());
+			this._removeBtn.addEventListener('click', () => {
+				this.remove();
+				this._inputGroup.remove();
 			});
-			list.querySelectorAll('.js-toggle-descriptions').forEach(toggleElm => toggleDescriptions(toggleElm, list));
-		});
-	};
+		}
+	}
 
-	/**
-	 * Creates another search input group when "Add" button is clicked.
+	class InputGroup {
+		constructor(elm, resultList) {
+			this._elm = elm;
+			this._resultList = resultList;
+			this.addEventListeners();
+		}
+
+		remove() {
+			this._elm.parentNode.removeChild(this._elm);
+		}
+
+		addEventListeners() {
+			this._elm.querySelector('.js-remove-from-search').addEventListener('click', () => {
+				this.remove();
+				this._resultList.remove();
+			});
+		}
+
+		get value() {
+			return this._elm.querySelector('.search').value;
+		}
+	}
+
+	class Search {
+		static searchValues = [];
+
+		constructor(resultList, inputGroup) {
+			this._resultList = new ResultList(resultList, inputGroup);
+			this._inputGroup = new InputGroup(inputGroup, this._resultList);
+			this.addToValues();
+		}
+
+		addToValues() {
+			Search.searchValues.push(this._inputGroup.value);
+		}
+	}
+
+  /**
+	 * Creates search input group when "Add" button is clicked.
  	 */
-	function createInputGroup() {
-		var inputGroup = document.createElement('div'),
+ const createInputGroup = () => {
+		let inputGroup = document.createElement('div'),
 				inputGroupAppend = document.createElement('div'),
 				newTermInput = document.createElement('input'),
 				remove = document.createElement('button'),
@@ -155,8 +151,8 @@
 		addMain.innerText = 'Add Main';
 		addMain.addEventListener('click', e => {
 			e.preventDefault();
-			var mainTerm = document.querySelector(`input[name="main"]`).value;
-			var newTerm = newTermInput.value;
+			let mainTerm = document.querySelector(`input[name="main"]`).value;
+			let newTerm = newTermInput.value;
 			if (mainTerm !== "") {
 				newTermInput.value = `${mainTerm} ${newTerm}`;
 			}
@@ -182,15 +178,6 @@
 	 * Triggers createInputGroup fn when "Add" button is clicked.
  	 */
 	document.getElementById('add-term').addEventListener('click', function() {
-		document.querySelector('.search-terms').appendChild(createInputGroup());
+		document.querySelector('.input-groups').appendChild(createInputGroup());
 	});
-
-	/**
-	 * Registers .result-list elements as draggable with "Move" button.
- 	 */
-	dragula([document.querySelector('.result-lists')],  { 
-		moves: function (el, container, handle) {
-			return handle.classList.contains('js-move');
-		}
-  });
 })();
